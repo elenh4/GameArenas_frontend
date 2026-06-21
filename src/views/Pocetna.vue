eda406ac3171fd97db81c09c67fff55deeebcc95
elenh4 <elenhaj77@gmail.com>
2026-06-21 22:15:27 +0200

    Update Pocetna.vue

--- src/views/Pocetna.vue
+++ src/views/Pocetna.vue
@@ -8,6 +8,7 @@
         <div style="text-align: center; margin: 5px 20px;">
           <img :src="logo" alt="Game Arenas" style="display: block; margin: 0 auto; max-width: 500px; width: 100%; height: auto;">
         </div>
+        <br>
         <span style="color: #00ffff; font-size: 12px; font-weight: bold; letter-spacing: 1px; cursor: pointer;" @click="idi_na_scoreboard">SCOREBOARD</span>
         <template v-if="jePrijavljen">
           <button @click="router.push('/ProfilKorisnik')" style="background: none; border: none; color: #00ffff; font-size: 12px; font-weight: bold; cursor: pointer;">PROFIL ({{ trenutniKorisnik.username }})</button>
